763ff8f86b4cad4573e140562204bdfdc46e8f7c
DominikBuchner <38790188+DominikBuchner@users.noreply.github.com>
2020-08-14 10:25:40 +0200

    update

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # BOLDigger-commandline
- BOLDigger as a commandline tool
+BOLDigger as a command-line tool for easy implementation into bioinformatic pipelines.
+
+Before using please check that you have updated to the latest version of BOLDigger since BOLDigger-cline partly relies on the current code of BOLDigger in large parts.
---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="boldigger-cline",
+    version="1.0.0",
+    author="Dominik Buchner",
+    author_email="dominik.buchner524@googlemail.com",
+    description="BOLDigger as a command-line tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DominikBuchner/BOLDigger-commandline",
+    packages=setuptools.find_packages(),
+    license = 'MIT',
+    install_requires = ['tqdm >= 4.48.2',
+                        'boldigger >= 1.1.9'],
+    include_package_data = True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    entry_points = {
+        "console_scripts" : [
+            "boldigger = boldigger-cline.__main__:main",
+        ]
+    },
+)
