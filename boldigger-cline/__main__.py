0adb19dd2e9a164f404877c5ad2abf8b6a2089ff
DominikBuchner <38790188+DominikBuchner@users.noreply.github.com>
2020-08-15 11:45:11 +0200

    Update __main__.py

--- boldigger-cline/__main__.py
+++ boldigger-cline/__main__.py
@@ -3,23 +3,46 @@ import pkg_resources, argparse
 ## import data needed for the requests
 certs = pkg_resources.resource_filename(__name__, 'data/certs.pem')
 
-## run only if called as a toplevel script
-if __name__ == "__main__":
-
+## main function to run and control the flow of bldigger-cline
+def main():
     ## collect and parse the cline arguments
-    parser = argparse.ArgumentParser(prog = 'BOLDigger commandline', description = 'BOLDigger as a command line tool')
-    subparsers = parser.add_subparsers()
+    parser = argparse.ArgumentParser(prog = 'boldigger-cline', description = 'BOLDigger as a command line tool, https://github.com/DominikBuchner/BOLDigger-commandline')
+    subparsers = parser.add_subparsers(dest = 'function')
 
+    ## group parsers that accept the same arguments
+    ## identification engines
     parser_bb_coi = subparsers.add_parser('bb_coi', help = 'COI identification engine')
     parser_bb_its = subparsers.add_parser('bb_its', help = 'ITS identification engine')
     parser_bb_rbcl = subparsers.add_parser('bb_rbcl', help = 'rbcl identification engine')
+
+    for subparser in [parser_bb_coi, parser_bb_its, parser_bb_rbcl]:
+        subparser.add_argument('username', help = 'Username for boldsystems login')
+        subparser.add_argument('password', help = 'Password for boldsystems login')
+        subparser.add_argument('fasta_path', help = 'Path to the fasta file to be blasted')
+        subparser.add_argument('output_folder', help = 'Path to the output folder')
+        subparser.add_argument('batch_size', type = int, default = 100, nargs = '?', help = 'Batch size to be blasted.')
+
+    ## metadata code
     parser_add_data = subparsers.add_parser('add_metadata', help = 'additional data download')
+
+    ## top hit selection
     parser_first_hit = subparsers.add_parser('first_hit', help = 'use the first hit as top hit')
     parser_jamp_hit = subparsers.add_parser('jamp_hit', help = 'determine the top hit with the JAMP method')
     parser_digger_hit = subparsers.add_parser('digger_hit', help = 'determine the top hit with the BOLDigger method')
+
+    for subparser in [parser_add_data, parser_first_hit, parser_jamp_hit, parser_digger_hit]:
+        parser_add_data.add_argument('xlsx_path', help = 'Path to the BOLDresults file')
+        parser_add_data.add_argument('output_folder', help = 'Path to the output folder')
+
 
     ## add version control
     parser.add_argument('--version', action='version', version= '1.0.0')
 
     ## parse the arguments
     args = parser.parse_args()
+
+
+
+## run only if called as a toplevel script
+if __name__ == "__main__":
+    main()
