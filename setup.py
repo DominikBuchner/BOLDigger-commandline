ab1813ec4c56e4338f40c084c4a816e8b77a8201
DominikBuchner <38790188+DominikBuchner@users.noreply.github.com>
2023-02-13 11:56:22 +0100

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="boldigger-cline",
-    version="2.0.1",
+    version="2.1.0",
     author="Dominik Buchner",
     author_email="dominik.buchner524@googlemail.com",
     description="BOLDigger as a command-line tool",
@@ -13,18 +13,17 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/DominikBuchner/BOLDigger-commandline",
     packages=setuptools.find_packages(),
-    license = 'MIT',
-    install_requires = ['tqdm >= 4.32.2',
-                        'boldigger >= 2.1.0'],
-    include_package_data = True,
+    license="MIT",
+    install_requires=["tqdm >= 4.32.2", "boldigger >= 2.1.1"],
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
-    entry_points = {
-        "console_scripts" : [
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
             "boldigger-cline = boldigger_cline.__main__:main",
         ]
     },
